90d0d08821d7e43a49398908226f23ed562854c2
pavladan <fatalfenker@gmail.com>
2019-06-27 15:40:05 +0300

    Change all paths to npm package

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const path = require("path");
+const {BrowserWindow, app} = require("electron");
+const {getPluginEntry} = require("mpv.js-vanilla");
+require("electron-debug")({showDevTools: false});
+
+const pdir = path.join(path.dirname(require.resolve("mpv.js-vanilla")), "build", "Release");
+if (process.platform !== "linux") {process.chdir(pdir);}
+app.commandLine.appendSwitch("ignore-gpu-blacklist");
+app.commandLine.appendSwitch("register-pepper-plugins", getPluginEntry(pdir));
+
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 574,
+    autoHideMenuBar: true,
+    useContentSize: process.platform !== "linux",
+    title: "mpv.js example player",
+    webPreferences: {plugins: true},
+  });
+  win.setMenu(null);
+  win.loadURL(`file://${__dirname}/index.html`);
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
